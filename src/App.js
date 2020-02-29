4099d48659b14a2bbe41cf61b7cf690efb40033e
Piyush <piyush.kotian@gmail.com>
2020-02-29 17:27:32 +0530

    Added barlow font and used typography

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import {
+  makeStyles,
+  createMuiTheme,
+  responsiveFontSizes,
+  ThemeProvider
+} from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+let theme = responsiveFontSizes(
+  createMuiTheme({
+    typography: {
+      fontFamily: "Barlow"
+    }
+  })
+);
 
 const useStyles = makeStyles(theme => ({
+  appBarTitleTypo: {
+    color: "#fafafa",
+    fontWeight: 700
+  },
   appBar: {
     background: "#212529"
   },
   root: {
     minHeight: "100vh",
     width: "100%",
-    background: "#212529"
-  }
+    background: "#212529",
+    padding: "2rem",
+    [theme.breakpoints.down("xs")]: {
+      padding: "1rem"
+    }
+  },
+  dashboardTitleTypo: {
+    color: "#fafafa",
+    fontWeight: 700
+  },
+  offset: theme.mixins.toolbar
 }));
 
 function App() {
@@ -22,17 +49,28 @@ function App() {
   return (
     <>
       <CssBaseline />
-      <AppBar className={classes.appBar}>
-        <Toolbar>Air Monitoring System</Toolbar>
-      </AppBar>
-      <div className={classes.root}>
-        <Grid container>
-          <Grid item xs={12}></Grid>
-          <Grid item xs={12}>
-            <Dashboard />
+      <ThemeProvider theme={theme}>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="h4" className={classes.appBarTitleTypo}>
+              Air Monitoring System
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <div className={classes.offset} />
+        <div className={classes.root}>
+          <Grid container>
+            <Grid item xs={12}>
+              <Typography variant="h3" className={classes.dashboardTitleTypo}>
+                Dashboard
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Dashboard />
+            </Grid>
           </Grid>
-        </Grid>
-      </div>
+        </div>
+      </ThemeProvider>
     </>
   );
 }
