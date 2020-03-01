bd03f536d181ddad45e8752fdb1a0e265fb5f41f
Piyush <piyush.kotian@gmail.com>
2020-03-01 15:12:30 +0530

    Added table and general fixes

--- src/App.js
+++ src/App.js
@@ -12,10 +12,53 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
 import useSocket from "use-socket.io-client";
+import MaterialTable from "material-table";
 
 import LiveAQI from "./LiveAQI";
 import AQIGraph from "./AQIGraph";
 import AQIForecast from "./AQIForecast";
+
+import { forwardRef } from "react";
+
+import AddBox from "@material-ui/icons/AddBox";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import Check from "@material-ui/icons/Check";
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+import ChevronRight from "@material-ui/icons/ChevronRight";
+import Clear from "@material-ui/icons/Clear";
+import DeleteOutline from "@material-ui/icons/DeleteOutline";
+import Edit from "@material-ui/icons/Edit";
+import FilterList from "@material-ui/icons/FilterList";
+import FirstPage from "@material-ui/icons/FirstPage";
+import LastPage from "@material-ui/icons/LastPage";
+import Remove from "@material-ui/icons/Remove";
+import SaveAlt from "@material-ui/icons/SaveAlt";
+import Search from "@material-ui/icons/Search";
+import ViewColumn from "@material-ui/icons/ViewColumn";
+
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => (
+    <ChevronRight {...props} ref={ref} />
+  )),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => (
+    <ChevronLeft {...props} ref={ref} />
+  )),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
 
 let theme = responsiveFontSizes(
   createMuiTheme({
@@ -54,7 +97,7 @@ function App() {
   const [aqiData, setAQIData] = useState([]);
   const [forecastValues, setForecastValues] = useState({});
   const [statusBrkPoints, setStatusBrkPoints] = useState([90, 120]);
-  const [aqiMinMax, setAQIMinMax] = useState([30, 90]);
+  const [aqiMinMax, setAQIMinMax] = useState([30, 150]);
   const [alertList, setAlertList] = useState([]);
 
   // const getRandomInt = (min, max) => {
@@ -87,7 +130,11 @@ function App() {
     console.log(inc_data);
     const aqiVal = parseInt(inc_data["aqi"]);
 
-    setAQIData(aqiData.concat(aqiVal));
+    // setAQIData(aqiData.concat(aqiVal));
+    aqiData.push(aqiVal);
+    if (aqiData.length > 8) {
+      aqiData.unshift();
+    }
     // setAQIData(aqiData.concat(1));
     console.log(aqiData);
 
@@ -96,8 +143,15 @@ function App() {
     if (aqiVal > statusBrkPoints[1]) {
       setAlertList(
         alertList.concat({
-          displayed: true,
-          aqi: aqiVal
+          aqi: aqiVal,
+          level: 1
+        })
+      );
+    } else if (aqiVal > statusBrkPoints[0]) {
+      setAlertList(
+        alertList.concat({
+          aqi: aqiVal,
+          level: 0
         })
       );
     }
@@ -130,6 +184,7 @@ function App() {
                 statusBrkPoints={statusBrkPoints}
                 forecastValues={forecastValues}
                 aqiMinMax={aqiMinMax}
+                alertList={alertList}
               />
             </Grid>
           </Grid>
@@ -140,13 +195,31 @@ function App() {
 }
 
 function Dashboard(props) {
-  const { aqiData, statusBrkPoints, forecastValues, aqiMinMax } = props;
+  const {
+    aqiData,
+    statusBrkPoints,
+    forecastValues,
+    aqiMinMax,
+    alertList
+  } = props;
 
   return (
     <Grid container>
       <Grid container spacing={2} item xs={12} sm={10}>
-        <Grid container direction="row-reverse" item xs={12}>
-          <Grid item xs={12} sm={9}></Grid>
+        <Grid container spacing={2} direction="row-reverse" item xs={12}>
+          <Grid item xs={12} sm={9}>
+            <div style={{ width: "100%" }}>
+              <MaterialTable
+                columns={[
+                  { title: "AQI", field: "aqi" },
+                  { title: "Warning Level", field: "level" }
+                ]}
+                data={alertList}
+                title="Alerts"
+                icons={tableIcons}
+              />
+            </div>
+          </Grid>
           <Grid item xs={12} sm={3}>
             <LiveAQI
               aqiMin={aqiMinMax[0]}
